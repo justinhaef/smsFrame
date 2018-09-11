5c44a914ab7287ab7f519be754451290333a8585
OwnsMinivan <justin.haefner@gmail.com>
2018-09-10 21:33:36 -0500

    added function comments.

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-import requests
 import os
 import json
 from datetime import datetime
 
+import requests
 import boto3
 from flask import Flask, request
 
@@ -23,9 +23,16 @@ s3 = boto3.resource('s3')
 # directory
 localDirectory = '/tmp/'
 
+# AWS S3 Bucket Name
+s3_bucketName = 'testingshirley'
+
 app = Flask(__name__)
 
 def download_files(media_files_list, _from):
+    ''' We need to download the files from Twilio using the Media URL. 
+        We'll identify the picture using the sending phone number and 
+        a time stamp.
+    '''
     for media in media_files_list:
         url = media[0]
         now = f"{datetime.now():%Y-%m-%d %H-%M-%S-%f}"
@@ -34,11 +41,18 @@ def download_files(media_files_list, _from):
             f.write(requests.get(url).content)
 
 def upload_files():
+    ''' After we've downloaded the pictures, we'll loop though and upload
+        them to AWS S3 bucket.
+    '''
     for root, dirs, files in os.walk(localDirectory):
         for filename in files:
-            s3.Bucket('testingshirley').upload_file(localDirectory + filename, filename)
+            s3.Bucket(s3_bucketName).upload_file(localDirectory + filename, filename)
 
 def checkNumber(_from):
+    ''' Make sure the sender is known, read the json
+        file of known senders and return the name if
+        it is found.
+    '''
     with open('whois.json', 'r') as f:
         knownNumbers = json.load(f)
         if _from in knownNumbers["from"]:
@@ -48,6 +62,14 @@ def checkNumber(_from):
 
 @app.route('/sms', methods=['GET', 'POST'])
 def get_details():
+    ''' Main logic of the app. Twilio webhook sends to this
+        function directly.  We'll pull out the important parts
+        of the message and loop though to get all the media URLs. 
+
+        Then we will check the trusted name list, if there is a match
+        we'll download the image so we can upload it to S3.  Then we'll
+        let the sender know we received the picture.
+    '''
     _from = request.values.get("From", None)
     _body = request.values.get("Body", None)
     _messagesid = request.values.get("MessageSid", None)
@@ -57,6 +79,7 @@ def get_details():
                     request.values.get("MediaContentType{}".format(i), None))
                         for i in range(0, num_media)]
     
+    # See if the sender is in the trusted list
     name = checkNumber(_from)
     if name != False:
         download_files(media_files_list, _from)
