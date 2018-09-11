a892f19bacf3900375e9898cf3c6bdff4df99e4f
OwnsMinivan <justin.haefner@gmail.com>
2018-09-10 21:03:18 -0500

    updated to use number as well as trusted list

--- run.py
+++ run.py
@@ -1,28 +1,25 @@
-from flask import Flask, request
-
 import requests
 import os
+import json
+from datetime import datetime
 
 import boto3
+from flask import Flask, request
 
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 
-import uuid # don't use this, use datetime instead.
-from datetime import datetime
-
 import instance.config as config
-
-# ToDo -> create a list of known people that are only allowed to send MMS.
 
 # Your Account SID from twilio.com/console
 account_sid = config.account_sid
 # Your Auth Token from twilio.com/console
 auth_token  = config.auth_token
+client = Client(account_sid, auth_token)
 
+# AWS S3 Resource
 s3 = boto3.resource('s3')
 
-client = Client(account_sid, auth_token)
 app = Flask(__name__)
 
 @app.route('/sms', methods=['GET', 'POST'])
@@ -36,22 +33,31 @@ def get_details():
                     request.values.get("MediaContentType{}".format(i), None))
                         for i in range(0, num_media)]
     
-    download_files(media_files_list)
-    upload_files()
+    name = checkNumber(_from)
+    if name != False:
+        download_files(media_files_list, _from)
+        upload_files()
 
-    print("From {} said: {} with this media URL: {}".format(_from, _body, media_files_list))
+        print("From {} said: {} with this media URL: {}".format(name, _body, media_files_list))
 
-    message = _from + ", thanks for the message!"
+        message = name + ", thanks for the message!"
 
-    resp = MessagingResponse()
-    resp.message(message)
+        resp = MessagingResponse()
+        resp.message(message)
 
-    return str(resp)
+        return str(resp)
+    else:
+        message = _from + ", sorry but you're not on the list!"
+        resp = MessagingResponse()
+        resp.message(message)
 
-def download_files(media_files_list):
+        return str(resp)
+
+def download_files(media_files_list, _from):
     for media in media_files_list:
         url = media[0]
-        messageId = str(uuid.uuid4())
+        now = f"{datetime.now():%Y-%m-%d %H-%M-%S-%f}"
+        messageId = str(_from + "-" + now)
         with open('/Users/justinhaefner/Desktop/Shirley/' + messageId + '.png', 'wb') as f:
             f.write(requests.get(url).content)
 
@@ -60,6 +66,14 @@ def upload_files():
     for root, dirs, files in os.walk(directory):
         for filename in files:
             s3.Bucket('testingshirley').upload_file(directory + filename, filename)
+
+def checkNumber(_from):
+    with open('whois.json', 'r') as f:
+        knownNumbers = json.load(f)
+        if _from in knownNumbers["from"]:
+            return knownNumbers["from"][_from]
+        else:
+            return False
 
 if __name__ == "__main__":
     app.run()
